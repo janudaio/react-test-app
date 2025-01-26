99dc1a9f876a6cbbec31222794ee1c558763e4d8
dev-januda <jaariyasinghe2003@gmail.com>
2025-01-26 23:49:00 +0530

    Did some changes to 'Home.jsx' and 'Home.css'

---
+++ src/components/Home.jsx
@@ -0,0 +1,67 @@
+import '../styles/home.css';
+
+function Home() {
+    return (
+        <>
+            <div className="main-section home">
+                <div className="home-content">
+                    <div>
+                        <span className="hello">Hello!</span>
+                        <h1>
+                            I am <br />
+                            <span>Januda</span>
+                        </h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Molestias consequatur
+                        </p>
+                    </div>
+                </div>
+                <div className="contacts">
+                    <ul>
+                        <li>dev.januda@gmail.com</li>
+                        <li>+94 75 71 98138</li>
+                        <li>Kegalle, Sri Lanka</li>
+                    </ul>
+                    <div className="icons">
+                        <a href="https://github.com/janudaio" target="_blank">
+                            <ion-icon name="logo-github"></ion-icon>
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/in/januda-amaneth-094124288/"
+                            target="_blank"
+                        >
+                            <ion-icon name="logo-linkedin"></ion-icon>
+                        </a>
+                        <a href="#" target="_blank">
+                            <ion-icon name="logo-google"></ion-icon>
+                        </a>
+                        <a
+                            href="https://www.instagram.com/__ja_n_u_wa.__/"
+                            target="_blank"
+                        >
+                            <ion-icon name="logo-instagram"></ion-icon>
+                        </a>
+                    </div>
+                    <div className="call2act">
+                        {/* <button className="interactive-hover-button-1">
+                            <a
+                                href=""
+                                style="color: inherit; text-decoration: none"
+                            >
+                                About Me
+                            </a>
+                        </button>
+                        <button className="interactive-hover-button-2">
+                            <a style="color: inherit; text-decoration: none">
+                                Portfolio
+                            </a>
+                        </button> */}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 // import './index.css';
 // import App from './App.jsx';
-import Test from './components/Test';
+import Home from './components/Home';
 // import Contacts from './components/Contacts';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <>
-            <Test />
+            <Home />
             {/* <App /> */}
         </>
     </StrictMode>
